9ad8883e1bc01c27cdb64b2a3020a5af3c27b6ec
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-21 12:55:26 +0000

    add : recipeを返すapiを追加

--- src/routers/recipe.py
+++ src/routers/recipe.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+from src.services.recipe import RecipeService
+from src.fetchers.recipe import RecipeFetcher
 
 router = APIRouter()
 
 @router.get('/recipes/{category_id}')
-async def get_recipes(category_id: str, num: int = 5):
-    pass
+async def get_recipes(category_id: str):
+    recipe_service = RecipeService(RecipeFetcher())
+    return recipe_service.fetch_recipes_by_category_ids([category_id])
 
