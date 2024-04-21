f1922a4ea3d0890ab5aaf4bfd3f592e11ddb3be7
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-21 06:44:45 +0000

    add : Redisへの登録処理と取得処理を追加

---
+++ src/cruds/category.py
@@ -0,0 +1,26 @@
+import redis
+from abc import ABC, abstractmethod
+from schemas.category import Category
+from src.services.category import ICategoryCRUD
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+class CategoryCRUD(ICategoryCRUD):
+    def cache_categories(self, categories: list[Category]):
+        for category in categories:
+            r.hset('categories', str(category.id), category.model_dump_json())
+            
+    def get_all_categories(self) -> list[Category]:
+        categories: list[Category] = []
+        
+        redis_categories = r.hgetall('categories')
+        
+        if not redis_categories:
+            pass
+        
+        for category_raw in redis_categories.values():
+            category = Category.model_validate(category_raw)
+            categories.append(category)
+            
+        return categories
+
--- src/services/category.py
+++ src/services/category.py
@@ -1,29 +1,51 @@
 from src.schemas.category import Category
 import src.config as config
-from typing import List
 import requests
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
-def fetch_categories() -> List[Category] | Exception:
-    try:
-        req = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?',
-                       params={
-                           'applicationId': config.RAKUTEN_APPLICATION_ID,
-                           'format': 'json',
-                           'categoryType': 'small',
-                           'elements': 'categoryId,categoryName,categoryUrl',
-                       })
+# データベースとやり取りするための抽象クラス
+class ICategoryCRUD(ABC):
+    @abstractmethod
+    def cache_categories(self, categories: list[Category]):
+        pass
     
-        data = req.json()
-        categories = data['result']['small']
+    @abstractmethod
+    def get_all_categories(self) -> list[Category]:
+        pass
     
-        # Categoryのスキーマに合うようにリネーム
-        for category in categories:
-            category['id'] = category.pop('categoryId')
-            category['name'] = category.pop('categoryName')
-            category['url'] = category.pop('categoryUrl')
+
+@dataclass
+class CategoryService():
+    category_crud: ICategoryCRUD
     
-        return categories
+    def __init__(self, category_crud: ICategoryCRUD):
+        self.category_crud = category_crud
+        
+    def fetch_categories(self) -> list[Category]:
+        try:
+            req = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?',
+                            params={
+                                'applicationId': config.RAKUTEN_APPLICATION_ID,
+                                'format': 'json',
+                                'categoryType': 'small',
+                                'elements': 'categoryId,categoryName,categoryUrl',
+                            })
     
-    # 一旦普通にエラーを投げる
-    except Exception as e:
-        raise e
+            data = req.json()
+            categories = data['result']['small']
+    
+            # Categoryのスキーマに合うようにリネーム
+            for category in categories:
+                category['id'] = category.pop('categoryId')
+                category['name'] = category.pop('categoryName')
+                category['url'] = category.pop('categoryUrl')
+                
+            # categoriesをリストに格納
+            categories = [Category(**category) for category in categories]
+   
+            return categories
+    
+        # 一旦普通にエラーを投げる
+        except Exception as e:
+            raise e
