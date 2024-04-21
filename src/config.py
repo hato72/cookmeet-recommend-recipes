4b361449215e93d76aa5981ffb42e6abad82e0cf
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-21 05:36:16 +0000

    add : configを定義し、エラーを返すようにした

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv('../.env')
+
+RAKUTEN_APPLICATION_ID = os.getenv('APPLICATION_ID')
+
--- src/services/category.py
+++ src/services/category.py
@@ -1,23 +1,29 @@
 from src.schemas.category import Category
+import src.config as config
 from typing import List
 import requests
 
-def fetch_category() -> List[Category]:
-    req = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?',
+def fetch_categories() -> List[Category] | Exception:
+    try:
+        req = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?',
                        params={
-                           'applicationId': '1069371834256835280',
+                           'applicationId': config.RAKUTEN_APPLICATION_ID,
                            'format': 'json',
                            'categoryType': 'small',
                            'elements': 'categoryId,categoryName,categoryUrl',
                        })
     
-    data = req.json()
-    categories = data['result']['small']
+        data = req.json()
+        categories = data['result']['small']
     
-    # Categoryのスキーマに合うようにリネーム
-    for category in categories:
-        category['id'] = category.pop('categoryId')
-        category['name'] = category.pop('categoryName')
-        category['url'] = category.pop('categoryUrl')
+        # Categoryのスキーマに合うようにリネーム
+        for category in categories:
+            category['id'] = category.pop('categoryId')
+            category['name'] = category.pop('categoryName')
+            category['url'] = category.pop('categoryUrl')
     
-    return categories
+        return categories
+    
+    # 一旦普通にエラーを投げる
+    except Exception as e:
+        raise e
