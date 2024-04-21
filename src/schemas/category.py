55e734b583eada0a2b46ce9d1859e97093789536
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-21 05:12:00 +0000

    fix : 正規表現のエラーを修正

--- src/schemas/category.py
+++ src/schemas/category.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel, Field
 class Category(BaseModel):
     id: int
     name: str
-    url: str = Field(..., title="URL", regex="https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+") # urlの正規表現
+    url: str = Field(..., title="URL", pattern="https?://[\\w!?/+\-_~;.,*&@#$%()'\\[\\]]+") # urlの正規表現
