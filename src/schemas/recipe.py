bfc2146587dc380b8b8cd8792ab50177204b19ac
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-21 12:00:02 +0000

    add : categoryIdからレシピを取得する関数を追加

--- src/schemas/recipe.py
+++ src/schemas/recipe.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from abc import ABC, abstractmethod
 
 class Recipe(BaseModel):
     id: int
@@ -7,3 +8,14 @@ class Recipe(BaseModel):
     description: str
     image_url: str = Field(..., title='Image URL', pattern="https?://[\\w!?/+\\-_~;.,*&@#$%()'\\[\\]]+")
     
+# レシピ取得用の抽象クラス
+class IRecipeFetcher(ABC):
+    @abstractmethod
+    def fetch_recipe(self, category_id: str) -> list[Recipe]:
+        """
+        レシピを取得する
+        
+        category_idは大-中-小の順で指定する
+        例: 10-200-2001
+        """
+        pass
