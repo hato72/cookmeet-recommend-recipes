67224588919c4d140c1215917bb0702358c5b3db
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-22 14:29:38 +0000

    add : recommendのAPIを追加

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from src.routers import category, recipe
+from src.routers import category, recipe, recommend
 
 app = FastAPI()
 
 app.include_router(category.router, prefix='/v1')
 app.include_router(recipe.router, prefix='/v1')
+app.include_router(recommend.router, prefix='/v1')
