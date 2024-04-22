694ca66e4b1bc42c7c1e181cf7d2705df4c8fcb1
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-22 11:38:50 +0000

    add : レシピ取得のapiを戻した

--- src/routers/recipe.py
+++ src/routers/recipe.py
@@ -6,6 +6,11 @@ from src.schemas.recommend import RecommendRequestBody
 
 router = APIRouter()
 
+@router.get('/recipes/{category_id}')
+async def get_recipes_by_category_id(category_id: CategoryId):
+    recipe_service = RecipeService(RecipeFetcher())
+    return recipe_service.fetch_recipes_by_category_ids([category_id])
+
 @router.post('/recipes/recommend')
 async def recommend_recipes(recommend_request_body: RecommendRequestBody):
     pass
