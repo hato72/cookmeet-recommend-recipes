c58b5e93094e707e070cc3cce887abcf317ebb9f
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-22 09:17:56 +0000

    add : categoryIdという型を定義

--- src/schemas/category.py
+++ src/schemas/category.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, Field
+from typing import NewType
+
+CategoryId = NewType('CategoryId', str)
 
 class Category(BaseModel):
-    id: str
+    id: CategoryId
     name: str
     url: str = Field(..., title="URL", pattern="https?://[\\w!?/+\\-_~;.,*&@#$%()'\\[\\]]+") # urlの正規表現
