381acfdfdc42f9ca860f15257caceee2336c9e50
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-22 07:22:51 +0000

    add : recommendの抽象クラスを作成

--- src/schemas/recommend.py
+++ src/schemas/recommend.py
@@ -1,5 +1,18 @@
 from pydantic import BaseModel
+from abc import ABC, abstractmethod
+from src.schemas.category import Category
+from src.schemas.recipe import Recipe
 
 class RecommendRequestBody(BaseModel):
     texts: list[str]
     conditions: list[str]
+    
+class Recommend(ABC):
+    
+    @abstractmethod
+    def recommend_categories(self, psychorogical_test_results: list[str], num_categories_to_recommend: int = 5) -> list[Category]:
+        pass
+    
+    @abstractmethod
+    def recommend_recipes(self, contditions: list[str], num_recipes_to_recommend: int = 3) -> list[Recipe]:
+        pass
