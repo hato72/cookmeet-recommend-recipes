384e1c09abda22d1373a905e12b85de5cfe2ea72
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-22 11:43:49 +0000

    change : recommend専用のルートを作成することにした

--- src/routers/recipe.py
+++ src/routers/recipe.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from src.services.recipe import RecipeService
 from src.fetchers.recipe import RecipeFetcher
 from src.schemas.category import CategoryId
-from src.schemas.recommend import RecommendRequestBody
 
 router = APIRouter()
 
@@ -10,8 +9,4 @@ router = APIRouter()
 async def get_recipes_by_category_id(category_id: CategoryId):
     recipe_service = RecipeService(RecipeFetcher())
     return recipe_service.fetch_recipes_by_category_ids([category_id])
-
-@router.post('/recipes/recommend')
-async def recommend_recipes(recommend_request_body: RecommendRequestBody):
-    pass
 
