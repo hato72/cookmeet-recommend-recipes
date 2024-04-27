cdf90b72f6fd280063597ba38a7c2b248b342b21
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-27 03:05:24 +0000

    add : レシピの手順の取得処理を追加

--- src/routers/recipe.py
+++ src/routers/recipe.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from src.services.recipe import RecipeService
 from src.fetchers.recipe import RecipeFetcher
 from src.schemas.category import CategoryId
+from src.scraping import scraping_reciep_steps
 
 router = APIRouter()
 
@@ -9,4 +10,13 @@ router = APIRouter()
 async def get_recipes_by_category_id(category_id: CategoryId):
     recipe_service = RecipeService(RecipeFetcher())
     return recipe_service.fetch_recipes_by_category_ids([category_id])
+
+@router.get('/recipes/{recipe_id}/steps')
+async def get_recipe_steps(recipe_id: int):
+    print (recipe_id)
+    recipe_steps = scraping_reciep_steps(recipe_id=recipe_id)
+    return {"recipe_id": recipe_id, "steps": recipe_steps}
+    
+    
+    
 
